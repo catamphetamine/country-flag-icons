aa583d364408e0e609f3824a5f0478669dfb4882
catamphetamine <purecatamphetamine@gmail.com>
2020-02-24 22:02:21 +0300

    Added `countries` export

--- test/exports.test.js
+++ test/exports.test.js
@@ -1,14 +1,17 @@
 import {
-	hasFlag
+	hasFlag,
+	countries
 } from '../index'
 
 describe('exports/core', () => {
 	it('should export ES6', () => {
 		hasFlag.should.be.a('function')
+		countries.includes('RU').should.equal(true)
 	})
 
 	it('should export CommonJS', () => {
 		const Library = require('../index.commonjs')
 		Library.hasFlag.should.be.a('function')
+		Library.countries.includes('RU').should.equal(true)
 	})
 })
